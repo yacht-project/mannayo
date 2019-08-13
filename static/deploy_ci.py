cab8638e5f6e29953fb433dbc9c033f12296cb08
JaeHyung Jang <jaehyung.jang@rgpkorea.co.kr>
2019-08-13 18:38:57 +0900

    Upload s3 command

---
+++ static/deploy_ci.py
@@ -0,0 +1,19 @@
+import boto3
+
+s3 = boto3.client('s3')
+
+response = s3.list_buckets()
+
+
+# 아직 다른게 없으니까 index.html 만 올립니다
+bucket_name = 'mannayo-test'
+filename = 'index.html'
+
+with open(filename, 'rb') as data:
+    s3.put_object(
+        Body=data,
+        Bucket=bucket_name,
+        Key=filename,
+        ContentType='text/html',
+        ACL='public-read',
+    )
