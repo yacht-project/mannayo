51f5f2fa4e69b84812da37cd7e05a718530759d2
Dongwook.kim <forestkeep21@naver.com>
2019-09-02 23:32:58 +0900

    [ADD] split into components from IndexPage

---
+++ front/src/components/WhereForm.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <p class="title">어디서 만나요?</p>
+        <div class="field has-addons has-addons-centered">
+            <div class="control">
+                <input class="input" type="text" placeholder="만날 장소를 입력해보세요.">
+            </div>
+        </div>
+    </div>
+</template>
---
+++ front/src/components/WhyForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p class="title">왜 만나요?</p>
+        <div class="field has-addons has-addons-centered">
+            <div class="control">
+                <div class="buttons">
+                    <a class="button is-outlined">회식</a>
+                    <a class="button is-outlined">티타임</a>
+                    <a class="button is-outlined">급벙개</a>
+                    <a class="button is-outlined">작당모의</a>
+                    <a class="button is-outlined">동호회</a>
+                    <a class="button is-outlined">가족모임</a>
+                    <a class="button is-outlined">소개팅</a>
+                    <a class="button is-outlined">식사</a>
+                    <a class="button is-outlined">여행</a>
+                    <a class="button is-outlined">기타</a>
+                </div>
+                <input class="input" type="text" placeholder="직접 입력">
+            </div>
+        </div>
+    </div>
+</template>
