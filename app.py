3d6097f918bcde62b25e7090b28ca6a14b2daef4
JaeHyung Jang <jaehyung.jang@rgpkorea.co.kr>
2019-10-08 16:09:53 +0900

    Create id from bread name

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import uuid
 import boto3
 
 from chalice import Chalice
+from chalicelib.ids import get_random_bread
 
 app = Chalice(app_name='mannayo')
 _DB = None
@@ -42,7 +43,8 @@ def create_meeting():
     json = app.current_request.json_body
 
     when = parse(json['when']).strftime('%Y-%m-%d')
-    title = uuid.uuid4().hex[:8]  # Create meeting_id
+    #title = uuid.uuid4().hex[:8]  # Create meeting_id
+    title = get_random_bread()  # Create meeting_id
     meeting = Meeting(
         title=title,
         when=when,
