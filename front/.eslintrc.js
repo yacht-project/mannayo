ea945233f1f5343956aa500155da3a91d5294be2
ChunKu Kim <chunku.kim@deliveryhero.co.kr>
2019-11-28 09:57:28 +0900

    chore(front): vue/max-attributes-per-line 린트 설정
    줄이 너무 길어지면 max-line-length가 작동 될 것이라 믿고
    한 줄에 여러 속성을 적어도 되도록 singleline 수정

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -12,6 +12,12 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "vue/max-attributes-per-line": ["error", {
+    "singleline": 10,
+    "multiline": {
+      "max": 1,
+      "allowFirstLine": false
+    }}],
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
