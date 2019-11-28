f5c10037e0ce5d742592670357b4c67b696bb333
ChunKu Kim <chunku.kim@deliveryhero.co.kr>
2019-11-28 09:57:47 +0900

    feat(front): 왜, 누구 버튼 동작

--- front/src/components/WhoForm.vue
+++ front/src/components/WhoForm.vue
@@ -6,23 +6,24 @@
     <div class="field has-addons has-addons-centered">
       <div class="control">
         <div class="buttons">
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWho">
             회사
           </button>
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWho">
             친구
           </button>
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWho">
             가족
           </button>
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWho">
             낯선사람
           </button>
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWho">
             기타
           </button>
         </div>
         <input
+          v-model="who"
           class="input"
           type="text"
           placeholder="직접 입력"
@@ -38,6 +39,14 @@ import Vue from 'vue';
 export default Vue.extend({
   name: 'WhoForm',
   props: {
+  },
+  data: () => ({
+    who: '',
+  }),
+  methods: {
+    selectWho(event: MouseEvent) {
+      this.$data.who = (event.target as HTMLButtonElement).innerText;
+    },
   },
 });
 </script>
--- front/src/components/WhyForm.vue
+++ front/src/components/WhyForm.vue
@@ -6,38 +6,39 @@
     <div class="field has-addons has-addons-centered">
       <div class="control">
         <div class="buttons">
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWhy">
             회식
           </button>
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWhy">
             티타임
           </button>
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWhy">
             급벙개
           </button>
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWhy">
             작당모의
           </button>
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWhy">
             동호회
           </button>
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWhy">
             가족모임
           </button>
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWhy">
             소개팅
           </button>
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWhy">
             식사
           </button>
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWhy">
             여행
           </button>
-          <button class="button is-outlined">
+          <button class="button is-outlined" @click="selectWhy">
             기타
           </button>
         </div>
         <input
+          v-model="why"
           class="input"
           type="text"
           placeholder="직접 입력"
@@ -53,6 +54,14 @@ import Vue from 'vue';
 export default Vue.extend({
   name: 'WhyForm',
   props: {
+  },
+  data: () => ({
+    why: '',
+  }),
+  methods: {
+    selectWhy(event: MouseEvent) {
+      this.$data.why = (event.target as HTMLButtonElement).innerText;
+    },
   },
 });
 </script>
